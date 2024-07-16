35f2df08b16aa0a0b610ef6a3a96a5f5659d19d7
Sai <Sai@DESKTOP-QEPBKRK>
2024-07-16 13:01:12 +0530

    made changes as per the comment

--- src/com/javaprograms/questions/CharecterOccurence.java
+++ src/com/javaprograms/questions/CharecterOccurence.java
@@ -5,32 +5,28 @@ import java.util.HashMap;
 public class CharecterOccurence {
 
 	public static void main(String[] args) {
-		
+
 		String str = "Ajaykumar";
-        HashMap<Character,Integer>  charCount = new HashMap<>();
+		HashMap<Character, Integer> charCount = new HashMap<>();
 
+		for (int i = 0; i < str.length(); i++) {
 
-       for (int i=0; i<str.length();i++) {
+			if (charCount.containsKey(str.charAt(i))) {
 
-       	if (charCount.containsKey(str.charAt(i))) {
+				int count = charCount.get(str.charAt(i));
+				System.out.println();
 
-       		int count = charCount.get(str.charAt(i));
-       		System.out.println();
+				charCount.put(str.charAt(i), ++count);
 
+			}
 
-       		charCount.put(str.charAt(i), ++count);
+			else {
+				charCount.put(str.charAt(i), 1);
 
-       	}
+			}
+		}
 
-       	else {
-       		charCount.put(str.charAt(i), 1);
-
-       	}
-       	}
-
-       System.out.println(charCount);
-       }
-
+		System.out.println(charCount);
 	}
 
-
+}
